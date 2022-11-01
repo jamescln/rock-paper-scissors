e4c2fb59651ca1366cc4348b8fd3692aa0f4499b
James <james.jcarline@gmail.com>
2022-11-01 13:59:50 +0000

    Create playRound function
    The playRound function will accept playerChoice and computerChoice
    as arguments, compare the two and then return the results.

--- main.js
+++ main.js
@@ -11,6 +11,22 @@ const getComputerChoice = function () {
   }
 };
 
-console.log(getComputerChoice());
-console.log(getComputerChoice());
-console.log(getComputerChoice());
+// Create a function that plays a single round of Rock, Paper, Scissors. The function must accept two parameters: playerSelection and computerSelection
+
+const playRound = function (playerSelection, computerSelection) {
+  if (
+    (playerSelection === "rock" && computerSelection === "paper") ||
+    (playerSelection === "paper" && computerSelection === "scissors") ||
+    (playerSelection === "scissors" && computerSelection === "rock")
+  ) {
+    return `Computer chooses ${computerSelection}. ${computerSelection} beats ${playerSelection}. You lose!`;
+  } else if (
+    (playerSelection === "rock" && computerSelection === "scissors") ||
+    (playerSelection === "paper" && computerSelection === "rock") ||
+    (playerSelection === "scissors" && computerSelection === "paper")
+  ) {
+    return `Computer chooses ${computerSelection}. ${playerSelection} beats ${computerSelection}. You win!`;
+  } else {
+    return `Not a valid option. Computer wins by default!`;
+  }
+};
