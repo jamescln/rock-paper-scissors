39d64f9988c60678d36b72145706bcd11cd25325
James <james.jcarline@gmail.com>
2022-11-06 17:48:07 +0000

    Consolidate global variables

--- main.js
+++ main.js
@@ -4,12 +4,10 @@ const playerChoiceButtons = document.querySelectorAll(".playerChoice");
 const resultText = document.querySelector(".result");
 const finalResultText = document.querySelector(".final-result");
 let playerChoice;
-
-// define playerScore and computerScore values
-
 let playerScore = 0;
 let computerScore = 0;
 let round = 0;
+let gameResult;
 
 // Begin with a function called getComputerChoice that randomly returns rock, paper or scissors.
 
@@ -69,9 +67,7 @@ const game = function () {
   finalResultText.textContent = `Current Score: Player ${playerScore} - Computer ${computerScore}`;
   round++;
   if (round === 5) {
-    console.log(
-      `Final Score: Player ${playerScore} - Computer ${computerScore}`
-    );
+    finalResultText.textContent = `Final Score: Player ${playerScore} - Computer ${computerScore}`;
     if (computerScore > playerScore) {
       console.log(`Computer Wins!`);
     } else if (computerScore < playerScore) {
